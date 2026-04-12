0d7e18970bfab0cf3af6c2e864a7fbc7a0c2f228
hr9919 <hrnam99@chungbuk.ac.kr>
2026-04-13 04:06:47 +0900

    (#13)week04: BOJ 1697 숨바꼭질, BOJ 7576 토마토

---
+++ HAERIM_NAM/week04/boj_7576_토마토.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int M, N;
+    static int[][] box;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static Queue<int[]> queue = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        box = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                box[i][j] = Integer.parseInt(st.nextToken());
+                if (box[i][j] == 1) {
+                    queue.add(new int[]{i, j});
+                }
+            }
+        }
+
+        System.out.println(bfs());
+    }
+
+    static int bfs() {
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int x = current[0];
+            int y = current[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx >= 0 && nx < N && ny >= 0 && ny < M && box[nx][ny] == 0) {
+                    box[nx][ny] = box[x][y] + 1;
+                    queue.add(new int[]{nx, ny});
+                }
+            }
+        }
+
+        int maxDays = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (box[i][j] == 0) return -1;
+                maxDays = Math.max(maxDays, box[i][j]);
+            }
+        }
+
+        return maxDays - 1;
+    }
+}
